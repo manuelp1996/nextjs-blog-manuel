9fb4fbda78f9ccd46d9f9a7860bdd2895d6ea810
Manuel Palma <mepr1996@gmail.com>
2022-02-07 15:26:22 -0300

    primer commit

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+    return (
+        <Layout
+            title="About | next.js"
+            description="Agrege una descripción">
+            <h1>About</h1>
+        </Layout>
+    )
+}
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import Layout from "../components/Layout";
+
+export default function contact() {
+    return (
+        <Layout
+            title="Contact | next.js"
+            description="Agrege una descripción">
+            <h1>Contact</h1>
+        </Layout>
+    )
+}
